e590eb3d73e9899c2194c5f34af45deb5806125d
MichaelMalcev <malcevm798@gmail.com>
2024-11-27 17:31:56 +0500

    27.11.2024

--- 12/14373.py
+++ 12/14373.py
@@ -1,1 +1,11 @@
-a
+t=[]
+for n in range(4,1000):
+    s = "3" + n * "5"
+    while "333" in s or "555" in s:
+        if "555" in s:
+            s = s.replace("555", "35", 1)
+        else:
+            s = s.replace("333", "53",1)
+    lena=len(s)
+    t.append(lena)
+print(max(t))
